b2578e74f0fa8d2295a6e33e9c69a222e2366c56
Ryan Ginnow <ryanmginnow@gmail.com>
2025-08-15 23:27:19 -0400

    allow path as argument; add comments

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sys
 from stats import count_words, count_character_usage, sort_character_usage_by_count
 
+# Get the contents found at the path provided
 def get_book_text(path):
     text = ''
     with open(path) as f:
@@ -7,32 +9,49 @@ def get_book_text(path):
 
     return text
 
+# Print the BookBot header
+# path is a string of the relative path to the book
 def report_header(path):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path}")
 
+# Print the word count section
+# count is an integer of the total words in the book
 def report_word_count(count):
     print("----------- Word Count ----------")
     print(f"Found {count} total words")
 
+# Print the character count section
+# character_list is a list of dictionaries of each character's total count
 def report_character_count(character_list):
     print("--------- Character Count -------")
+
+    # Loop each dictionary in the list
     for item in character_list:
         character = item["char"]
         count = item["count"]
         print(f"{character}: {count}")
 
+# End the report
 def report_footer():
     print("============= END ===============")
 
 def main():
-    path = "books/frankenstein.txt"
+    # If we don't get 2 arguments, show command usage
+    if len(sys.argv) != 2:
+        print ("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
+    # Get path from arguments
+    path = sys.argv[1]
+
+    # Gather data about the book from the path
     book_text = get_book_text(path)
     word_count = count_words(book_text)
     char_count = count_character_usage(book_text)
     char_usage = sort_character_usage_by_count(char_count)
 
+    # Build the report
     report_header(path)
     report_word_count(word_count)
     report_character_count(char_usage)
--- stats.py
+++ stats.py
@@ -1,13 +1,17 @@
-def sort_by_count(items):
-    return items["count"]
-
+# Count the number of words in the book
+# text is the full text content of the book
 def count_words(text):
     return len(text.split())
 
+# Count the number of times a character is used in the book
+# text is the full text content of the book
 def count_character_usage(text):
     character_list = list(text)
-
     character_dict = {}
+
+    # Loop each character in the book text
+    # and append it to the dictonary if it doesn't exist
+    # or increase its count value by 1 if it already does
     for c in character_list:
         character = c.lower()
         if character in character_dict:
@@ -17,10 +21,19 @@ def count_character_usage(text):
 
     return character_dict
 
+# Define the key to sort by in the list of dictionaries
+# item is a dictonary
+def sort_by_count(item):
+    return item["count"]
+
+# Get the count of characters, sorted from high to low
+# usage_dict is a dictonary of <character>: <count>
 def sort_character_usage_by_count(usage_dict):
     character_list = list(usage_dict)
-
     dict_list = []
+
+    # Loop each character in the list
+    # and append the specific dictionary structure to the list
     for c in character_list:
         if c.isalpha():
             count = usage_dict[c]
