b06e6edf523ea907d22f918143103fe5f5da39f1
Ryan Ginnow <ryanmginnow@gmail.com>
2025-08-15 22:01:39 -0400

    count character usage

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_character_usage
 
 def get_book_text(path):
     text = ''
@@ -9,8 +9,6 @@ def get_book_text(path):
 
 def main():
     book_text = get_book_text('./books/frankenstein.txt')
-    word_count = count_words(book_text)
-
-    print(f"{word_count} words found in the document")
+    print(count_character_usage(book_text))
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,16 @@
 def count_words(text):
     return len(text.split())
+
+def count_character_usage(text):
+    char_list = list(text)
+    char_count = len(char_list)
+
+    characters = {}
+    for i in range(char_count):
+        char = char_list[i].lower()
+        if char in characters:
+            characters[char] = characters[char] + 1
+        else:
+            characters[char] = 1
+
+    return characters
